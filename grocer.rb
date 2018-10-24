9d3c1305989965626aece12760ea952674b7371c
Emily Asch <e.asch@me.com>
2018-10-24 23:34:39 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,16 +1,6 @@
 def consolidate_cart(cart:[])
   # code here	  
-  con_hash = {}
-  cart.each do |item|
-    item.each do |name, attribute|
-      if con_hash.has_key?(name)
-        con_hash[name][:count] += 1        
-      else 
-        con_hash = con_hash.merge({name => attribute.merge({count: 1})})
-      end
-    end
-  end
-  return con_hash
+
 end	
 
 def apply_coupons(cart:[], coupons:[])
