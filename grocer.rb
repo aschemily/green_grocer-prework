4226b185e44e67bbe30c54b4697341ecde66d3f0
Emily Asch <e.asch@me.com>
2018-10-24 23:26:16 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,11 @@
-def consolidate_cart(cart)
-  # code here
-end
-
-def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
-end
+def consolidate_cart(cart:[])
+  # code here	  new_hash = {}
+  cart.each do |item|
+    item.each do |food, item|
+      item[:count] ||= 0
+      item[:count] += 1
+      new_hash[food] = item
+    end
+  end
+    new_hash
+end	end
