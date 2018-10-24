a16a2d1ec748b0c8de70aaf1ac39213b30b94728
Emily Asch <e.asch@me.com>
2018-10-24 23:35:32 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,32 @@ def consolidate_cart(cart:[])
 end	
 
 def apply_coupons(cart:[], coupons:[])
-
+ new_cart = {}
+  
+  cart.each do |food_item, info_hash|
+    new_cart[food_item] = info_hash
+    
+    coupons.each do |coupon_hash|
+      
+      if food_item == coupon_hash[:item]
+        
+        if info_hash[:count] >= coupon_hash[:num]
+          info_hash[:count] -= coupon_hash[:num]
+          
+          if new_cart.include?(food_item+" W/COUPON")
+            new_cart[food_item+" W/COUPON"][:count] += 1
+          else
+            new_cart[food_item+" W/COUPON"] = {
+                :price => coupon_hash[:cost],
+                :clearance => info_hash[:clearance],
+                :count => 1
+            }
+          end
+        end
+      end
+    end
+  end
+   new_cart
 end	
 
 def apply_clearance(cart:[])
